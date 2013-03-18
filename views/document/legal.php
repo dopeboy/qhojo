50153ced9dc1413d8b3f0a54e5eda6fbf0ad8102
Manish <manish@manish-laptop.localhost>
2013-03-17 23:48:59 -0400

    legal page added, reserve page tidied

---
+++ views/document/legal.php
@@ -0,0 +1,26 @@
+<div id="masterdiv">
+    <div id="mainheading">
+        How it works
+    </div>
+    <hr/>
+    <div class="subheading">
+        Terms & Conditions
+    </div>
+    <div class="subcontent">
+        blah blah blah
+    </div>
+    <br/>
+    <div class="subheading">
+        Privacy Policy
+    </div>
+    <div class="subcontent">
+        blah blah blah
+    </div>
+    <br/>
+    <div class="subheading">
+        Usage Policy
+    </div>
+    <div class="subcontent">
+        blah blah blah
+    </div>    
+</div>
