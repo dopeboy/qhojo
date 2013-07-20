d508bd5251323e0061c16bc8b43e5ea0a9eb3015
Manish Sinha <manish@qhojo.com>
2013-07-19 20:05:05 -0400

    again

--- views/user/join.php
+++ views/user/join.php
@@ -1,13 +1,44 @@
-<div class="subcontainer" id="join" style="">
-        <form class="form-join">
+<div class="sheet " id="join" style="">
+        <form class="form-join" id="signup">
             <legend>Join</legend>	
-            <input type="text" class="input-block-level" placeholder="First Name">
-            <input type="text" class="input-block-level" placeholder="Last Name">
-            <input type="text" class="input-block-level" placeholder="Zip Code">            
-            <input type="text" class="input-block-level" placeholder="Email Address">
-            <input type="password" class="input-block-level" placeholder="Password">
-            <p><button class="btn btn-large btn-primary" type="submit">Sign up</button></p>
-    </form>    
+            
+            <div class="control-group">
+                <div class="controls">
+                    <input type="text" class="input-block-level" placeholder="First Name" id="fname" name="fname">
+                </div>
+            </div>                           
+                  
+                <div class="control-group">
+                    <div class="controls">
+                        <input type="text" class="input-block-level" placeholder="Last Name" id="lname" name="lname">
+                    </div>
+		</div>                       
+                        
+                <div class="control-group">
+                    <div class="controls">
+                        <input type="text" class="input-block-level" placeholder="Zip Code" id="zipcode" name="zipcode">    
+                    </div>
+		</div>      
+            
+                <div class="control-group">
+                    <div class="controls">
+                         <input type="text" class="input-block-level" placeholder="Email Address" id="email" name="email">    
+                    </div>
+		</div>                  
+            
+                <div class="control-group">
+                    <div class="controls">
+                         <input type="password" class="input-block-level" placeholder="Password" id="passwd" name="passwd"> 
+                    </div>
+		</div>                  
+
+		<div class="control-group">
+                    <label class="control-label" for="input01"></label>
+                    <div class="controls">
+                        <button type="submit" class="btn btn-large btn-primary" rel="tooltip" title="first tooltip">Create My Account</button>
+                    </div>
+                </div>
+        </form>    
 </div>
 
 
--- views/user/login.php
+++ views/user/login.php
@@ -1,12 +1,34 @@
-<div class="subcontainer" id="signin" style="">
-        <form class="form-signin">
-            <legend>Login</legend>	
-            <input type="text" class="input-block-level" placeholder="Email address">
-            <input type="password" class="input-block-level" placeholder="Password">
-            <label class="checkbox">
+<div class="sheet" id="login" style="">
+    <form class="form-login" id="login">
+        <legend>Login</legend>	
+        
+        <div class="control-group">
+            <div class="controls">
+                 <input type="text" class="input-block-level" placeholder="Email address" id="email" name="email">  
+            </div>
+        </div>   
+        
+        <div class="control-group">
+            <div class="controls">
+                   <input type="password" class="input-block-level" placeholder="Password" id="passwd" name="passwd">
+            </div>
+        </div>   
+        
+        <div class="control-group">
+            <div class="controls">
+                <label class="checkbox">
                 <input type="checkbox" value="remember-me"> Remember me
-            </label>
-            <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+                </label>
+            </div>
+        </div>         
+
+        <div class="control-group">
+            <label class="control-label" for="input01"></label>
+            <div class="controls">
+            <button type="submit" class="btn btn-large btn-primary" rel="tooltip" title="first tooltip">Create My Account</button>
+            </div>
+        </div>
+        
     </form>    
 </div>
 
