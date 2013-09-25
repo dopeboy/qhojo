5b401bfd4704bea4b2e4c134ff9d0e27b5cd03da
Manish Sinha <manish@qhojo.com>
2013-09-25 17:49:40 -0400

    fix share tiwtter

--- public_html/js/user/dashboard.js
+++ public_html/js/user/dashboard.js
@@ -67,7 +67,9 @@ $(document).ready(function()
     {
       var left = (screen.width/2)-(w/2);
       var top = (screen.height/2)-(h/2);
-      return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+      var window = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+      window.focus();
+      return window;
     } 
 
     $(".share.twitter").click(function() 
