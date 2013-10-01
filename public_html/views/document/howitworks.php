1ca26776f49f3990e734bb7b28f1d9a8f54f91d8
Manish Sinha <manish@qhojo.com>
2013-10-01 18:08:25 -0400

    fix 10

--- public_html/views/document/howitworks.php
+++ public_html/views/document/howitworks.php
@@ -1,244 +1,170 @@
 <title>Qhojo - How it works / FAQ</title>
 
 <div class="sheet">
-    <legend>How it works</legend>
-    
-    <table style=" margin: 0px auto; " class="table table-striped table-bordered text-center">
-        <th style="">
-            Lender
-        </th>
-        <th style="">
+    <h1 class="text-center">How it works</h1>
+    <hr>
+    <table id="steps" class="table">
+        <thead >
+            <tr  >
+                <th style="" class="lender">
+                <h2>Lender</h2>
+                </th>
+                <th class="step">
+                    <h2>Step</h2>
+                </th>
+                <th class="borrower">
+                    <h2>Borrower</h2>
+                </th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <tr style='height: 20px'>
+                
+            </tr>
+            <tr style="" class="">
+                <td class="left-cell" style="">
+                    <div class="tag left" style="">
+                        <div class="block" style="">
+                            <h3>List Item</h3>
+                            <p>
+                                Specify a borrow rate, a hold value, and toss in some descriptive details. Then post it on Qhojo (for free!).
+                            </p>   
+                        </div>
+                        <div class="arrow" style=""></div>
+                    </div>
+                </td>
+                <td class="middle-cell">
+                    <h1>1</h1>
+                </td>
+                <td  class="right-cell">                   
+                </td>
+            </tr>  
             
-        </th>
-        <th style="">
-            Borrower
-        </th>
-        <tr style="height: 50px;" >
-            <td>
+            <tr style="" class="">
+                <td class="left-cell" style="">
+              
+                </td>
+                <td class="middle-cell">
+                    <h1>2</h1>
+                </td>
+                <td class="right-cell">
+                    <div class="tag right">
+                        <div class="arrow"></div>
+                        <div class="block">
+                            <h3>Request Item</h3>
+                            <p>
+                                Submit a request to borrow an item for a specific duration.
+                            </p>      
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            
+            <tr style="" class="">
+                <td class="left-cell" style="">
+                    <div class="tag left" style="">
+                        <div class="block" style="">
+                            <h3>Approve Request</h3>
+                            <p>
+                                Sort through the borrow requests and approve one. If you decide to change your mind later on, you can cancel your approval up until 24 hours before the start of the borrow duration.
+                            </p>   
+                        </div>
+                        <div class="arrow" style=""></div>
+                    </div>
+                </td>
+                <td class="middle-cell">
+                    <h1>3</h1>
+                </td>
+                <td  class="right-cell">                   
+                </td>
+            </tr>      
+            
+            <tr style="" class="">
+                <td class="left-cell" style="">
+              
+                </td>
+                <td class="middle-cell">
+                    <h1>4</h1>
+                </td>
+                <td class="right-cell">
+                    <div class="tag right">
+                        <div class="arrow"></div>
+                        <div class="block">
+                            <h3>Pick Up</h3>
+                            <p>
+                                Schedule a time and place with the lender to pick up the item. Once you meet them, verify that the item is in the same condition as listed on Qhojo. Then, text us the confirmation code. We'll place a hold on your credit card for the hold amount.
+                            </p>      
+                        </div>
+                    </div>
+                </td>
+            </tr>     
+            
+            <tr style="" class="">
+                <td class="left-cell" style="">
+                    <div class="tag left" style="">
+                        <div class="block" style="">
+                            <h3>Release Item</h3>
+                            <p>
+                                We'll text you confirming that the borrower is OK with starting the transaction. At this point, you can hand the item over to the borrower.
+                            </p>   
+                        </div>
+                        <div class="arrow" style=""></div>
+                    </div>
+                </td>
+                <td class="middle-cell">
+                    <h1>5</h1>
+                </td>
+                <td  class="right-cell">                   
+                </td>
+            </tr>         
+            
+            <tr style="" class="">
+                <td class="left-cell" style="">
+              
+                </td>
+                <td class="middle-cell">
+                    <h1>6</h1>
+                </td>
+                <td class="right-cell">
+                    <div class="tag right">
+                        <div class="arrow"></div>
+                        <div class="block">
+                            <h3>Return Item</h3>
+                            <p>
+                                After you are done using the item, schedule a time and place with the lender to return the item on the due date. Don't be late or else you'll be subject to our late policy.
+                            </p>      
+                        </div>
+                    </div>
+                </td>
+            </tr>      
+            
+            <tr style="" class="">
+                <td class="left-cell" style="">
+                    <div class="tag left" style="">
+                        <div class="block" style="">
+                            <h3>Accept Item</h3>
+                            <p>
+                                Inspect the item to make sure it is OK. Then, text the confirmation code back to us. At this point, the hold will be released on the borrower's credit card and you will receive a payment from us.
+                            </p>   
+                            
+                            <p>
+                                In the event that there is damage, submit a damage report through the dashboard.
+                            </p>
+                        </div>
+                        <div class="arrow" style=""></div>
+                    </div>
+                </td>
+                <td class="middle-cell">
+                    <h1>7</h1>
+                </td>
+                <td  class="right-cell">                   
+                </td>
+            </tr>                     
+        
 
-                
-                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                    
-                </div>
-                <div class="arrow-right" style="float: left"></div>
-            </td>
-            <td>1</td>
-            <td></td>
-        </tr>
-        
-        <tr style="height: 50px;">
-            <td>
-             
-            </td>
-            <td>2</td>
-            <td>            
-            
-                
-                <div class="arrow-left" style="float: left"></div>
-                                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                    <strong> Request item:</strong>
-                sdfsdf 
-                </div>
-                
-            
-            </td>
-            
-            
-        </tr>
-        
-        <tr style="height: 50px;">
-            <td>
-                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                    <strong>Accept request:</strong>
-                    sdfsdf 
-                </div>
-                <div class="arrow-right" style="float: left"></div>
-                
-                  
-            </td>
-            <td>3</td>
-            <td></td>
-        </tr>
-        
-        <tr style="height: 50px;">
-            <td>
-              
-            </td>
-            <td>4</td>
-            <td>     
-            
-                <div class="arrow-left" style="float: left"></div>
-                                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                       <strong>Pick up item:</strong>
-                sdfsdf     
-                </div>                
-            
-            </td>
-        </tr>
-        
-        <tr style="height: 50px;">
-            <td>
- 
-                
-                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                        <strong>Release item:</strong>
-                sdfsdf  
-                </div>
-                <div class="arrow-right" style="float: left"></div>                
-            </td>
-            <td>5</td>
-            <td></td>
-        </tr>
-        
-        <tr style="height: 50px;">
-            <td>
-              
-            </td>
-            <td>6</td>
-            <td>         
-            
-                <div class="arrow-left" style="float: left"></div>
-                                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                      <strong>Return item:</strong>
-                sdfsdf         
-                </div>                 
-            </td>
-        </tr>     
-        
-        <tr style="height: 50px;">
-            <td>
- 
-                
-                <div style="height: 120px; width: 300px; float: left; background-color: green; ">
-                     <strong>Accept item:</strong>
-                sdfsdf   
-                </div>
-                <div class="arrow-right" style="float: left"></div>                   
-            </td>
-            <td>7</td>
-            <td>             </td>
-        </tr>          
+        </tbody>
     </table>
-    
-<!--    <div class="row-fluid text-center">
-        <div class="span5">
-            <strong><u>Lender</u></strong>
-        </div>
-        
-        <div class="span2">
-            
-        </div>
-        
-        <div class="span5">
-            <strong><u>Borrower</u></strong>
-        </div>
-    </div>
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-            <strong>List item:</strong>
-                sdfsdf
-        </div>
-        
-        <div class="span2">
-            1
-        </div>
-        
-        <div class="span5">
-            
-        </div>
-     </div>
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-
-        </div>
-        
-        <div class="span2">
-            2
-        </div>
-        
-        <div class="span5">
-            <strong> Request item:</strong>
-                sdfsdf            
-        </div>
-     </div>    
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-            <strong>Accept request:</strong>
-                sdfsdf  
-        </div>
-        
-        <div class="span2">
-            3
-        </div>
-        
-        <div class="span5">
-          
-        </div>
-     </div>  
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-
-        </div>
-        
-        <div class="span2">
-            4
-        </div>
-        
-        <div class="span5">
-            <strong>Pick up item:</strong>
-                sdfsdf            
-        </div>
-     </div>     
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-            <strong>Release item:</strong>
-                sdfsdf   
-        </div>
-        
-        <div class="span2">
-            5
-        </div>
-        
-        <div class="span5">
-         
-        </div>
-     </div>         
-    
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-
-        </div>
-        
-        <div class="span2">
-            6
-        </div>
-        
-        <div class="span5">
-            <strong>Return item:</strong>
-                sdfsdf            
-        </div>
-     </div>    
-    
-     <div class="row-fluid text-center">
-        <div class="span5">
-            <strong>Accept item:</strong>
-                sdfsdf   
-        </div>
-        
-        <div class="span2">
-            7
-        </div>
-        
-        <div class="span5">
-         
-        </div>
-     </div>        -->
     
 </div>
 
