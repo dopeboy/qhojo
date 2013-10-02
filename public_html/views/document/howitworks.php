aef24577b49a16ae5bb5694b4560296508c85c18
Manish Sinha <manish@qhojo.com>
2013-10-01 21:14:50 -0400

    how it works 4

--- public_html/views/document/howitworks.php
+++ public_html/views/document/howitworks.php
@@ -3,7 +3,7 @@
 <div class="sheet">
     <h1 class="text-center">How it works</h1>
     <hr>
-    <table id="steps" class="table">
+    <table id="steps" class="table table-striped">
         <thead >
             <tr  >
                 <th style="" class="lender">
@@ -19,7 +19,7 @@
         </thead>
         
         <tbody>
-            <tr style='height: 20px'>
+            <tr style='height: 10px'>
                 
             </tr>
             <tr style="" class="">
@@ -54,7 +54,7 @@
                         <div class="block">
                             <h3>Request Item</h3>
                             <p>
-                                Submit a request to borrow an item for a specific duration.
+                                Search for an item to borrow. Once you find it, submit a request to borrow it for a specific duration.
                             </p>      
                         </div>
                     </div>
@@ -67,7 +67,7 @@
                         <div class="block" style="">
                             <h3>Approve Request</h3>
                             <p>
-                                Sort through the borrow requests and approve one. If you decide to change your mind later on, you can cancel your approval up until 24 hours before the start of the borrow duration.
+                                Sort through the borrow requests you have received and approve one. If you decide to change your mind later on, you can cancel your transaction up until 24 hours before the start of the borrow duration.
                             </p>   
                         </div>
                         <div class="arrow" style=""></div>
@@ -93,7 +93,7 @@
                         <div class="block">
                             <h3>Pick Up</h3>
                             <p>
-                                Schedule a time and place with the lender to pick up the item. Once you meet them, verify that the item is in the same condition as listed on Qhojo. Then, text us the confirmation code. We'll place a hold on your credit card for the hold amount.
+                                Schedule a time and place with the lender to pick up the item. Once you meet them, verify that the item is in the same condition as listed on Qhojo. Then, text your confirmation code to the following number: <a href="tel:<?php global $borrower_number; echo $borrower_number; ?>"><?php global $borrower_number; echo $borrower_number; ?></a>. We'll place a hold on your credit card for the hold amount.
                             </p>      
                         </div>
                     </div>
@@ -106,7 +106,7 @@
                         <div class="block" style="">
                             <h3>Release Item</h3>
                             <p>
-                                We'll text you confirming that the borrower is OK with starting the transaction. At this point, you can hand the item over to the borrower.
+                                You'll receive a text message from us confirming that the borrower is OK with starting the transaction. At this point, you can hand the item over to the borrower.
                             </p>   
                         </div>
                         <div class="arrow" style=""></div>
@@ -130,10 +130,13 @@
                     <div class="tag right">
                         <div class="arrow"></div>
                         <div class="block">
-                            <h3>Return Item</h3>
+                            <h3>Use Item</h3>
                             <p>
-                                After you are done using the item, schedule a time and place with the lender to return the item on the due date. Don't be late or else you'll be subject to our <a href='#'>late policy</a>.
-                            </p>      
+                                Rock out with your borrowed gear! 
+                            </p>
+                            <p>
+                               Also schedule a time and place with the lender to return the item on the due date. Don't be late or else you'll be subject to our <a href='#'>late policy</a>.
+                            </p>
                         </div>
                     </div>
                 </td>
@@ -143,13 +146,13 @@
                 <td class="left-cell" style="">
                     <div class="tag left" style="">
                         <div class="block" style="">
-                            <h3>Accept Item</h3>
+                            <h3>Verify Item</h3>
                             <p>
-                                Inspect the item to make sure it is OK. Then, text the confirmation code back to us. At this point, the hold will be released on the borrower's credit card and you will receive a payment from us.
+                                Inspect the item to make sure it is OK. Then, text your confirmation code to the following number: <a href="tel:<?php global $lender_number; echo $lender_number; ?>"><?php global $lender_number; echo $lender_number; ?></a>. We'll send you the money you are owed minus a <a href="/document/fees">small transaction fee</a> to your PayPal account. 
                             </p>   
                             
                             <p>
-                                In the event that there is damage, submit a damage report through the dashboard.
+                                In the event that there is damage, do not send the text message and instead, <a href='#'>submit a damage report</a>.
                             </p>
                         </div>
                         <div class="arrow" style=""></div>
@@ -160,7 +163,27 @@
                 </td>
                 <td  class="right-cell">                   
                 </td>
-            </tr>                     
+            </tr>
+            
+            <tr style="" class="">
+                <td class="left-cell" style="">
+              
+                </td>
+                <td class="middle-cell">
+                    <h1>8</h1>
+                </td>
+                <td class="right-cell">
+                    <div class="tag right">
+                        <div class="arrow"></div>
+                        <div class="block">
+                            <h3>Return Item</h3>
+                            <p>
+                                Once you receive a text message from us confirming that the lender is OK with the returned item, return the item to the lender. We will also release the hold on your credit card and charge you for the borrow duration.
+                            </p>
+                        </div>
+                    </div>
+                </td>
+            </tr>              
         
 
         </tbody>
