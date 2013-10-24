d55f95b45877e98fe90c54b1b289a5b67b0310be
Manish Sinha <manish@qhojo.com>
2013-10-24 15:26:49 -0400

    about page 1

--- public_html/views/document/about.php
+++ public_html/views/document/about.php
@@ -6,15 +6,24 @@
     <h1 class="text-center">About</h1>
     <hr>
     <div id='about-text'>
-        <strong>Qhojo</strong> (pronounced <em>kō-ˈjō</em>) is a community of professionals who borrow camera gear from one another. We created Qhojo because we want to help photographers and videographers save on their rentals while also give them a way to make money with their unused gear.
+        <strong>Qhojo</strong> (pronounced <em>kō-ˈjō</em>) is a community for professionals to borrow camera gear from one another. We created Qhojo because we want to help photographers and videographers save on their rentals while also give them a way to make money with their unused gear.
     </div>
 </div>
 
-<!--<div class='sheet' id='team'>
+<div class='sheet' id='team'>
     <h1 class="text-center">Team</h1>
+     <hr>
     <div id='team-content'>
-        Picture, Picture' blurb underneath; linkedin + twitter underneath
-    </div>
-    <hr>
-        
-</div>-->
+        <div class="row-fluid">
+             <div class="span6 text-center">
+                     <img src="/img/m.jpg">
+                     <h3>Manish Sinha</h3>
+                     <p>I like building.</p>
+             </div>
+             <div class="span6 text-center">
+                 <img src="/img/g.jpg">
+                 <h3>Guido Lang</h3>
+                 <p>I like talking.</p>
+             </div>            
+    </div>     
+</div>
